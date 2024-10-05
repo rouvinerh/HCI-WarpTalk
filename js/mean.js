3964f8409a21757e3c1753ce2457dbbf85b4da65
Link <61148194+Linkbreathe@users.noreply.github.com>
2024-10-05 09:44:36 +0200

    Multi-Room & Chat
    Functionalities:
    1. Enables the creation of multiple rooms
    2. Enables receiving and sending messages in multiple rooms at the same time
    
    UI:
    1. Modified the overall style so that the height of the chat area fills the entire screen
    
    Description:
    Implemented functionality for creating multiple rooms and handling communication within them. Refactored message handling, room switching, and client display for better modularity. Added chat-box management with dynamic room selection and ensured support for multiple concurrent rooms. Enhanced drag-and-drop feature for chat-box positioning and improved message sending for each room individually.

---
+++ js/mean.js
@@ -0,0 +1,9 @@
+// const msgInputElement = document.getElementById("msg-input");
+//     msgInputElement.addEventListener("keydown", (e) => {
+//         if (e.key === "Enter") {
+//             e.preventDefault();
+//             const message = msgInputElement.value.trim();
+//             current_room.send(message);
+//             msgInputElement.value = '';
+//         }
+//     });
